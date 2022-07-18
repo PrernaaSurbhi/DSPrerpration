88fefbcc1133a4667313f59f5c49abe637a156d8
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2022-07-18 21:54:17 +0530

    Improve sliding window problem for solving max subArray of fixed size.

--- app/src/main/java/com/example/SlidingWindow/MaxSubArOfFixedSize.java
+++ app/src/main/java/com/example/SlidingWindow/MaxSubArOfFixedSize.java
@@ -6,31 +6,26 @@ package com.example.SlidingWindow;
 
 public class MaxSubArOfFixedSize {
     public static void main(String[] args) {
-        int[] arr = {2, 5, 1, 8, 2, 9, 1};
+        int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
         int windowSize = 3;
         //  maxSum(arr,windowSize);
         System.out.println(maxSum(arr, windowSize));
     }
 
-    public static int maxSum(int[] ar, int windowSize) {
-        int st = 0;
-        int e = 0;
-        int sum = 0;
+    public static int maxSum(int[] ar, int k) {
+        int n = ar.length;
+        int max_value = Integer.MIN_VALUE;
+        int current_runSum = 0;
 
-        for (int i = 0; i < ar.length; i++) {
-            if (e  <= windowSize) {
-                sum = sum + ar[i];
-                e++;
-            }
-            else if (e == windowSize) {
-                sum = sum - ar[i];
-                st++;
-                e++;
+        for(int i = 0 ; i< n; i++){
+            current_runSum += ar[i];
+
+            if(i >= k - 1){
+                max_value = Math.max(max_value,current_runSum);
+                current_runSum = current_runSum - ar[i - (k - 1)];
             }
         }
-
-        System.out.println("final is :" + sum);
-        return sum;
+        return max_value;
     }
 
 }
