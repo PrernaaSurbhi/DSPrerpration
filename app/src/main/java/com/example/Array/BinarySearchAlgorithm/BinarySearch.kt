084cf6cfb5f2a07f5e9ef3436f628fea82c02af1
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-08-10 14:25:10 +0530

    Binary Search to find index element

---
+++ app/src/main/java/com/example/Array/BinarySearchAlgorithm/BinarySearch.kt
@@ -0,0 +1,33 @@
+package com.example.Array.BinarySearchAlgorithm
+
+import java.util.Arrays
+
+object BinarySearch {
+    @JvmStatic
+    fun main(args: Array<String>){
+        val nums = arrayOf(4,5,6,7,0,1,2)
+        val target = 0
+        Arrays.sort(nums)
+        println("target index is :" + findindex(nums, target))
+
+    }
+
+    fun findindex(nums:Array<Int>,target:Int):Int{
+        var s = 0;
+        var e = nums.size-1
+
+        while(s<=e){
+            var mid = (s + e) / 2
+
+            if(target == nums[mid]){
+                return mid
+            }else if(target > nums[mid]){
+                s = mid + 1
+            }else{
+                e = mid -1
+            }
+        }
+
+        return -1
+    }
+}
